dd1b4130c71b6fee885023c59629fe23c853e15d
OrnellaNiyibizi <o.mahoro@gmail.com>
2022-10-31 07:17:40 -0700

    commit

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,4 +4,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     li = len(tuple_b)
     sum1 = (tuple_a[0] if le > 0 else 0) + (tuple_b[0] if li > 0 else 0)
     sum2 = (tuple_a[1] if le > 1 else 0) + (tuple_b[1] if li > 1 else 0)
-    return (sum1, sum2i)
+    return (sum1, sum2)
