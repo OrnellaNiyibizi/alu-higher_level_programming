a44fbc0ff7ea48adc9faa25f9bc12924611e028e
OrnellaNiyibizi <109486545+OrnellaNiyibizi@users.noreply.github.com>
2022-12-08 18:22:13 +0200

    Update rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -95,7 +95,8 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}" \
             .format(self.id, self.x, self.y, self.width, self.height)
     
-     def update(self, *args, **kwargs):
+
+    def update(self, *args, **kwargs):
         """assigns an argument"""
         if len(args) != 0:
             try:
