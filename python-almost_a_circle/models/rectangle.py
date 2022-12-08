626356fa14d68d1a946e899c7ebc50fe4a81c8be
OrnellaNiyibizi <109486545+OrnellaNiyibizi@users.noreply.github.com>
2022-12-08 18:27:15 +0200

    Update rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -93,7 +93,8 @@ class Rectangle(Base):
     def __str__(self):
         """returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
         return "[Rectangle] ({}) {}/{} - {}/{}" \
-            .format(self.id, self.x, self.y, self.width, self.height
+            .format(self.id, self.x, self.y, self.width, self.height)
+    
 
     def update(self, *args, **kwargs):
         """assigns an argument"""
