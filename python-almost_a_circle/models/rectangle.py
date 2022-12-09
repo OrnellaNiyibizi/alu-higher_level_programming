8cfae5c072d8243ef594d98b40b6fead72fc8661
OrnellaNiyibizi <109486545+OrnellaNiyibizi@users.noreply.github.com>
2022-12-09 09:27:11 +0000

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -31,7 +31,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Width setter"""
-        if type (value) is not int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
