35a7a7579a76a9a324f2b73c2be50dd61ba7d113
OrnellaNiyibizi <109486545+OrnellaNiyibizi@users.noreply.github.com>
2022-12-09 16:45:36 +0200

    summative

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-"""change the name of a State object from the database hbtn_0e_6_usa
-"""
-
-import sys
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+""" module update a state """
 
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    from sys import argv
+    from model_state import State, Base
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
     Base.metadata.create_all(engine)
-
-    state = session.query(State).filter(State.id == 2).first()
-
-    state.name = "New Mexico"
-
+    st = session.query(State).filter_by(id=2).first()
+    st.name = "New Mexico"
     session.commit()
     session.close()
