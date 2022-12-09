e793603ffaa38ac00741e89dae6eb7e53665028a
OrnellaNiyibizi <109486545+OrnellaNiyibizi@users.noreply.github.com>
2022-12-09 16:42:10 +0200

    summative

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""add the State object
-“Louisiana” to the database hbtn_0e_6_usa
-"""
-
-import sys
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-
+"""adds a new state"""
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sqlalchemy.schema import Table
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    state = State(name="Louisiana")
-    session.add(state)
+    session = Session(engine)
+    new = State(name='Louisiana')
+    session.add(new)
+    new_state = session.query(State).filter(State.name == 'Louisiana').first()
     session.commit()
-    print(state.id)
+    print("{}".format(new_state.id))
     session.close()
