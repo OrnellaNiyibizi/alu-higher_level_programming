88409fb6354ab351723e41ca8dd30dcfb96467c7
OrnellaNiyibizi <109486545+OrnellaNiyibizi@users.noreply.github.com>
2022-12-09 16:32:39 +0200

    summative

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-'''defing the SQL Alchemy using a database class'''
-
+""" model state database declaration """
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
+
+
 Base = declarative_base()
 
 
-class City(Base):
-    '''A class that defines of a table in the sql'''
-    __tablename__ = 'cities'
-
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+class State(Base):
+    """ state class table """
+    # declare table name
+    __tablename__ = 'states'
+    # define colum names and characteristics
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
