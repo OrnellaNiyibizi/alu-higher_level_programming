ddfd32b4481408aca3e26b7bd7b6e6762128c249
OrnellaNiyibizi <o.mahoro@alustudent.com>
2023-02-06 17:15:08 +0200

    gjk

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -6,19 +6,19 @@ class Rectangle {
       this.height = h;
     }
   }
-  
+
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
-  
+
   rotate () {
     this.width += this.height;
     this.height = this.width - this.height;
     this.width -= this.height;
   }
-  
+
   double () {
     this.width = this.width * 2;
     this.height = this.height * 2;
